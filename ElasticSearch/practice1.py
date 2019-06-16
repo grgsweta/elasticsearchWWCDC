803337f4dcc665cc777d9ed28fd0ebde2af2c174
Brianna McGowan <43475983+brm2398@users.noreply.github.com>
2019-06-16 17:08:57 -0400

    Update practice.py

--- ElasticSearch/practice1.py
+++ ElasticSearch/practice1.py
@@ -1,21 +1,30 @@
-from datetime import datetime
+################
+##Elastic Search
+################
+import requests, json, os
 from elasticsearch import Elasticsearch
-es = Elasticsearch()
 
-doc = {
-    'author': 'kimchy',
-    'text': 'Elasticsearch: cool. bonsai cool.',
-    'timestamp': datetime.now(),
-}
-res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
-print(res['result'])
+directory = '/path/to/files/'
 
-res = es.get(index="test-index", doc_type='tweet', id=1)
-print(res['_source'])
+#ElasticSearch can be accessed via localhost:9200 on the browser
 
-es.indices.refresh(index="test-index")
+res = requests.get('http://localhost:9200')
+print (res.content)
+es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
 
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
-print("Got %d Hits:" % res['hits']['total']['value'])
-for hit in res['hits']['hits']:
-    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+#Create an index value object
+
+i = 1
+
+#Iterate over each JSON file and import it into Elasticsearch
+
+for shakespeare in os.listdir(directory):
+    if shakespeare.endswith(".json"):
+        f = open(shakespeare)
+        docket_content = f.read()
+        # Send the data into es
+        es.index(index='myindex', ignore=400, doc_type='docket', 
+        id=i, body=json.loads(docket_content))
+        i = i + 1
+        
+        
